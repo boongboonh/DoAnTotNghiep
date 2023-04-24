ebfcef89a9709ddee6a9a11eda75eda287ed7133
boongboonh <nguyenvanbinhtcdb@gmail.com>
2023-04-24 17:29:55 +0700

    fix sound effect

--- Assets/AttackEndBehaviour.cs
+++ Assets/AttackEndBehaviour.cs
@@ -8,6 +8,7 @@ public class AttackEndBehaviour : StateMachineBehaviour
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         WeaponAttack.instance.canReceiveInput = true;
+        PlayerSounds.instance.SwordChopAudio();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
