b81d47de11ef0b22638dcc2627ddeda9676d2d14
boongboonh <nguyenvanbinhtcdb@gmail.com>
2023-04-26 00:18:46 +0700

    add story

--- Assets/Scripts/story/StoryRun.cs
+++ Assets/Scripts/story/StoryRun.cs
@@ -2,65 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class StoryRun : MonoBehaviour
 {
-
     public GameObject dialoguePanel;
-    public TextMeshProUGUI dialogueText; // vawn banr ddoi thoai
-    public string[] dialogue;
-
-
+    public TextMeshProUGUI dialogueText; // van ban chay
+    public List<string> dialogue;
 
     private int index = 0;
-
     public float wordSpeed;
+    public GameObject speedUpButton;
 
-    public GameObject skipButton;
+    private bool runNextText;
 
     [Header("sound text story")]
     [SerializeField] private AudioSource textSound;
 
-
     void Start()
     {
+        runNextText = false;
         dialogueText.text = "";
+        StartCoroutine(Typing());
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            StartCoroutine(Typing());
-            if (dialogueText.text == dialogue[index])
-            {
-                NextLine();
-            }
-        }
+        if (!runNextText) return;    //neu khong cho chay tiep cau tiep theo
+        
+        NextLine();
     }
 
-    //tat hoi thoai
-    //tao chu hien thi tu tu
+    // tao chu hien thi tu tu
     IEnumerator Typing()
     {
+        runNextText = false;
+
         foreach (char letter in dialogue[index].ToCharArray())
         {
             dialogueText.text += letter;
 
-            //am thanh chay chu
+            // am thanh chay chu
             textSound.Play();
 
             yield return new WaitForSeconds(wordSpeed);
         }
+
+        runNextText = true;
     }
 
     public void NextLine() // chuyen sang cau tiep theo
     {
-        if (index < dialogue.Length - 1)
+        if (index < dialogue.Count - 1)
         {
             index++;
-            dialogueText.text = "";
+            dialogueText.text += "\n";
             StartCoroutine(Typing());
         }
+        else
+        {
+            //chuyen canh 
+            runNextText = false;
+            Debug.Log("end story");
+
+            Invoke("ChangeScene", 2f);    //chuyen canh sau 2 giay
+        }
+    }
+
+    //chuyen canh
+    private void ChangeScene()
+    {
+        Debug.Log("chuyen den story 1");
+    }
+
+    //goi tron button
+    public void SpeedUpButton()
+    {
+        wordSpeed = 0;
+        speedUpButton.SetActive(false);
     }
 }
+
+
 
